830bc698b81a198efc35cdd419b31f9357d98b54
Lars Michael Kristensen <lmkr@hvl.no>
2019-08-29 22:16:49 +0200

    Revisjon F08 eksempler

--- forelesninger/F08MetoderI/src/no/hvl/dat100/EnMetodeMain.java
+++ forelesninger/F08MetoderI/src/no/hvl/dat100/EnMetodeMain.java
@@ -1,24 +1,20 @@
 package no.hvl.dat100;
 
-public class MetodeEksempel {
+public class EnMetodeMain {
 
 	public static void main(String[] args) {
 		
-		System.out.println("En setning før første løkke");
+		for (int i = 1; i<=10; i++) {
+			System.out.print("*");
+		}
+		System.out.println();
+		
+		System.out.println("-"); 
 		
 		for (int i = 1; i<=10; i++) {
 			System.out.print("*");
 		}
 		System.out.println();
 		
-		System.out.println("En setning mellom de to løkker");
-		
-		for (int i = 1; i<=10; i++) {
-			System.out.print("*");
-		}
-		System.out.println();
-		
-		System.out.println("En setning før programmet er slutt");
 	}
-
 }
